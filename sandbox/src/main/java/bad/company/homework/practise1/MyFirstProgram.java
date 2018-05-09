6cf9170bd2d059187d54dbd48379ab008ced209d
badroots <vasilkovanastusha@gmail.com>
2018-05-09 18:52:45 +0300

    practise funcs

--- sandbox/src/main/java/bad/company/homework/practise1/MyFirstProgram.java
+++ sandbox/src/main/java/bad/company/homework/practise1/MyFirstProgram.java
@@ -2,10 +2,22 @@ package bad.company.homework.practise1;
 
 public class MyFirstProgram {
 
-	public static void main(String[] args){  
+	public static void main(String[] args){
 
-		System.out.println("Hello");
+		double a = 2;
+		double b = 3;
+    System.out.println(area(a,b));
 
+    System.out.println(area(b));
+
+    }
+
+	public static double area(double len) {
+		return len * len;
+	}
+
+	public static double area(double a, double b) {
+		return a * b;
 	}
 
 }
