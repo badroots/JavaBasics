7a02a0aaae7203673d933d81432ff77d5bfeae76
badroots <vasilkovanastusha@gmail.com>
2018-05-11 20:22:51 +0300

    add new module for tests

--- sandbox/src/main/java/bad/company/homework/practise1/Operations.java
+++ sandbox/src/main/java/bad/company/homework/practise1/Operations.java
@@ -4,11 +4,11 @@ public class Operations {
 
   public static void main(String[] args) {
 
-//    Square s = new Square(5);
-//    System.out.println(s.area());
-//
-//    Rectangle r = new Rectangle(4,6 );
-//    System.out.println(r.area());
+    Square s = new Square(5);
+    System.out.println(s.area());
+
+    Rectangle r = new Rectangle(4,6 );
+    System.out.println(r.area());
 
     Point p1 = new Point(2,3);
     Point p2 = new Point(4,5);
--- sandbox/src/test/java/bad/company/homework/practise1/PointTests.java
+++ sandbox/src/test/java/bad/company/homework/practise1/PointTests.java
@@ -14,24 +14,24 @@ public class PointTests {
 
   @Test
   public void dxCountResultTest() {
-      Point p1 = new Point(2, 4);
-      Point p2 = new Point(6, 8);
-      double dx = p2.x - p1.x;
-      Assert.assertEquals(dx ,4.0 );
+    Point p1 = new Point(2, 4);
+    Point p2 = new Point(6, 8);
+    double dx = p2.x - p1.x;
+    Assert.assertEquals(dx ,4.0 );
   }
 
-    @Test
-    public void dyCountResultTest() {
-        Point p1 = new Point(2, 4);
-        Point p2 = new Point(6, 8);
-        double dy = p2.y - p1.y;
-        Assert.assertEquals(dy ,4.0);
+  @Test
+  public void dyCountResultTest() {
+    Point p1 = new Point(2, 4);
+    Point p2 = new Point(6, 8);
+    double dy = p2.y - p1.y;
+    Assert.assertEquals(dy ,4.0);
     }
 
   @Test
   public void distanceNotNull() {
-      Point p1 = new Point(2, 4);
-      Point p2 = new Point(6, 8);
+    Point p1 = new Point(2, 4);
+    Point p2 = new Point(6, 8);
     Assert.assertNotNull(p1.distance(p2), "Looks like smth go wrong");
   }
 
