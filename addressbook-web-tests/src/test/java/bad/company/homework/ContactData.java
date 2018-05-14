6bc20c4deb92caf82dca1cc22d889b75951f9f45
badroots <vasilkovanastusha@gmail.com>
2018-05-14 20:39:35 +0300

    add tests

---
+++ addressbook-web-tests/src/test/java/bad/company/homework/ContactData.java
@@ -0,0 +1,79 @@
+package bad.company.homework;
+
+public class ContactData {
+  private final String firstname;
+  private final String middlename;
+  private final String lastname;
+  private final String nickname;
+  private final String title;
+  private final String company;
+  private final String address;
+  private final String homePhoneNumber;
+  private final String mobilePhoneNumber;
+  private final String email;
+  private final String address2;
+  private final String notes;
+
+  public ContactData(String firstname, String middlename, String lastname, String nickname, String title, String company, String address, String homePhoneNumber, String mobilePhoneNumber, String email, String address2, String notes) {
+    this.firstname = firstname;
+    this.middlename = middlename;
+    this.lastname = lastname;
+    this.nickname = nickname;
+    this.title = title;
+    this.company = company;
+    this.address = address;
+    this.homePhoneNumber = homePhoneNumber;
+    this.mobilePhoneNumber = mobilePhoneNumber;
+    this.email = email;
+    this.address2 = address2;
+    this.notes = notes;
+  }
+
+  public String getFirstname() {
+    return firstname;
+  }
+
+  public String getMiddlename() {
+    return middlename;
+  }
+
+  public String getLastname() {
+    return lastname;
+  }
+
+  public String getNickname() {
+    return nickname;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public String getCompany() {
+    return company;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getHomePhoneNumber() {
+    return homePhoneNumber;
+  }
+
+  public String getMobilePhoneNumber() {
+    return mobilePhoneNumber;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public String getAddress2() {
+    return address2;
+  }
+
+  public String getNotes() {
+    return notes;
+  }
+}
